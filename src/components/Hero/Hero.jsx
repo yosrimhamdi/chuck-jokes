2f85d6815876567d0d68b6f1181bad4b31bf5bbb
Yosri Mhamdi <yosri@mhamdi.co>
2022-07-16 16:57:24 +0100

    Responsive hero section.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+import './Hero.scss';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <div>
+        <h1 className="hero__title">The Joke Bible</h1>
+        <h4 className="hero__sub-title">Daily Laughs for you and yours</h4>
+        <div className="hero__input-container">
+          <input
+            type="text"
+            className="hero__search-input"
+            placeholder="How can we make you laugh today?"
+          />
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="hero__magnifier"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
